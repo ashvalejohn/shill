61620bb58e0e0185f48e57e6d8e9dd094a4f6e9c
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-04 11:01:52 -0800

    Change shill to background.js

--- background.js
+++ background.js
@@ -7,10 +7,22 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
         new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { urlContains: 'www.youtube.com' }
+          pageUrl: { urlContains: 'www.youtube.com' },
+          css: ["#description"]
         })
       ],
       actions: [ new chrome.declarativeContent.ShowPageAction() ]
-    }]);
+    }], () => (console.log("On youtube!")));
   });
 });
+
+// STEP 2
+// Grab all links in the #description box
+const desc = window.document;
+console.log(desc);
+
+// STEP 3
+// Make API calls to each, return og: tag information
+
+// STEP 4
+// Display product cards
