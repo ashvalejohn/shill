4c2b7aefb8f3933174ec5cc4102df29bf92c24c1
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-05 10:13:26 -0800

    Grab array of property tags and their content as an object

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 // STEP 1
 // Open extension if user is on a YouTube page
 chrome.runtime.onInstalled.addListener(function () {
-  console.log("RUNNING background.js");
-
+  console.log("RUN background.js");
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
@@ -19,23 +18,36 @@ chrome.runtime.onInstalled.addListener(function () {
 
 // STEP 4
 // Receive product link URLs, send xhr request for og: tags
-let urls = [];
-
 chrome.runtime.onConnect.addListener(function (port) {
-  console.log(port.name);
-  port.onMessage.addListener(function (msg) {
-    urls = msg;
-  });
-
-  console.log(urls);
-
-  // const xhr = new XMLHttpRequest();
-  // xhr.open('GET', 'http://api.example.com/data.json', true);
-  // xhr.onreadystatechange = function () {
-  //   if (xhr.readyState == 4) {
-  //     // JSON.parse does not evaluate the attacker's scripts.
-  //     var resp = JSON.parse(xhr.responseText);
-  //   }
-  // }
-  // xhr.send();
+  console.log(`shill.js started port ${port.name}`);
+  port.onMessage.addListener((msg) => (
+    msg.urls.map((url) => {
+      const req = new XMLHttpRequest();
+      req.open('GET', `${url}`, true);
+      req.responseType = "document";
+      req.onreadystatechange = function () {
+        console.log(`Sending request to: ${url}`);
+        if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+          // JSON.parse does not evaluate the attacker's scripts.
+          let ogInfo = [];
+          const doc = req.response;
+          if (doc) {
+            const meta = doc.querySelectorAll('meta');
+            meta.forEach((tag) => {
+              if (tag.hasAttribute("property")) {
+                let props = {};
+                const attrs = Array.from(tag.attributes);
+                attrs.map((attr) => {
+                  props[attr.name] = attr.value;
+                });
+                ogInfo.push(props);
+              }
+            });
+          }
+          console.log(ogInfo);
+        }
+      }
+      req.send();
+    })
+  ));
 });
--- shill.js
+++ shill.js
@@ -1,23 +1,17 @@
-console.log("RUNNING shill.js");
+console.log("RUN shill.js");
 const port = chrome.runtime.connect({ name: "shill" });
 
 // STEP 2
 // Grab all links in the #description box
-// const sendUrls = setTimeout(() => { 
-//   const links = Array.from(document.querySelectorAll('#description > a'));
-//   const urls = links.map(link => (
-//     link.textContent
-//   ));
-
-//   // STEP 3
-//   // Send URLs to background as message
-//   port.postMessage({ urls: urls });
-// }, 5000);
 const desc = Array.from(document.querySelectorAll("#description > a"));
 const links = desc.map(link => (
   link.textContent
 ));
-console.log(links);
+
+
+// STEP 3
+// Send URLs to background as message
+port.postMessage({ urls: links });
 
 
 
