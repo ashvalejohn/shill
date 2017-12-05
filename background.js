a33201115c11b21326d202452d4a011cfa438774
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-04 22:46:41 -0800

    Call shill.js from background.js after pageAction loads, kill setTimeout bug

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 // Open extension if user is on a YouTube page
 chrome.runtime.onInstalled.addListener(function () {
   console.log("RUNNING background.js");
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
@@ -10,18 +11,31 @@ chrome.runtime.onInstalled.addListener(function () {
           css: ["#description"]
         })
       ],
-      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      actions: [ new chrome.declarativeContent.ShowPageAction(), 
+      new chrome.declarativeContent.RequestContentScript( { "js": ["shill.js"] }) ]
     }]);
   });
 });
 
-
 // STEP 4
-// Receive product link URLs, send AJAx request for og: tags
+// Receive product link URLs, send xhr request for og: tags
+let urls = [];
+
 chrome.runtime.onConnect.addListener(function (port) {
   console.log(port.name);
   port.onMessage.addListener(function (msg) {
-    console.log(msg.urls[8]);
-
+    urls = msg;
   });
+
+  console.log(urls);
+
+  // const xhr = new XMLHttpRequest();
+  // xhr.open('GET', 'http://api.example.com/data.json', true);
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState == 4) {
+  //     // JSON.parse does not evaluate the attacker's scripts.
+  //     var resp = JSON.parse(xhr.responseText);
+  //   }
+  // }
+  // xhr.send();
 });
--- shill.js
+++ shill.js
@@ -3,18 +3,21 @@ const port = chrome.runtime.connect({ name: "shill" });
 
 // STEP 2
 // Grab all links in the #description box
-const sendUrls = setTimeout(() => { 
-  const links = Array.from(document.querySelectorAll('#description > a'));
-  const urls = links.map(link => (
-    link.textContent
-  ));
-  console.log(urls);
-  port.postMessage({ urls: urls });
-}, 5000);
+// const sendUrls = setTimeout(() => { 
+//   const links = Array.from(document.querySelectorAll('#description > a'));
+//   const urls = links.map(link => (
+//     link.textContent
+//   ));
 
-
-// // STEP 3
-// Send URLs to background as message
+//   // STEP 3
+//   // Send URLs to background as message
+//   port.postMessage({ urls: urls });
+// }, 5000);
+const desc = Array.from(document.querySelectorAll("#description > a"));
+const links = desc.map(link => (
+  link.textContent
+));
+console.log(links);
 
 
 
