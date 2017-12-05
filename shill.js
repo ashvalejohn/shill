11d7621ef2f4e578d534a65c43d1a783f81d1512
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-04 16:58:55 -0800

    Grab just URL text

--- shill.js
+++ shill.js
@@ -1,11 +1,18 @@
 // STEP 2
 // Grab all links in the #description box
 console.log("RUNNING shill.js");
-const links = setTimeout(() => { console.log(document.querySelectorAll('#description > a'));}, 5000);
 
+const sendUrls = setTimeout(() => { 
+  const links = Array.from(document.querySelectorAll('#description > a'));
+  const urls = links.map(link => (
+    link.textContent
+  ));
+  return urls;
+}, 2000);
 
 // // STEP 3
 // Send URLs to background as message
+
 
 // STEP 5
 // Display product cards
