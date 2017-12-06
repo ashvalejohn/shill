27712a693790cba658ffc5b162dae5f6298a0161
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-05 22:23:03 -0800

    Render link cards with ~*style*~

--- shill.js
+++ shill.js
@@ -1,9 +1,11 @@
 console.log("RUN shill.js");
 const port = chrome.runtime.connect({name: 'shill'})
+const desc = document.getElementById("description");
+// const shillStyle = chrome.runtime.getURL;
+document.querySelector('head').insertAdjacentHTML('afterbegin', `<link rel='stylesheet' type='text/css' href='${chrome.runtime.getURL("shill-style.css")}'>`);
 
 // STEP 2
 // Grab all links in the #description box
-const desc = document.getElementById("description");
 const descLinks = Array.from(document.querySelectorAll("#description > a"));
 const links = descLinks.map(link => (
   link.textContent
@@ -19,28 +21,25 @@ port.postMessage({ urls: links });
 
 // STEP 6
 // Receive card information
-let cards = [];
 port.onMessage.addListener((card) => {
   const cardInfo = {};
   const ogTags = card.metaTags.filter(attrs => attrs.property.includes('og:'));
+
   ogTags.forEach( tag => {
     if (tag.property === 'og:title') {
         cardInfo['title'] = tag.content;
     } else if (tag.property === 'og:image') {
-        cardInfo['img-src'] = tag.content;
+        cardInfo['imgSrc'] = tag.content;
     } else if (tag.property === 'og:description') {
       cardInfo['description'] = tag.content;
     }
   });
-  console.log(cardInfo);
-  // card.map((link) => {
-  //   desc.insertAdjacentHTML('afterbegin', `<div>${link.content}</div>`)
-  // })
+
+  desc.insertAdjacentHTML('afterbegin', `<div class='shill-card'><img src='${cardInfo.imgSrc}'<h1>${cardInfo.title}</h1><p>${cardInfo.description}</p></div>`);
 });
+
 
 
 // STEP 7
 // Display product cards
 const cardTemplate = document.createElement('div');
-
-desc.insertAdjacentHTML('afterbegin', "<div id='shill-cards' style='background-color: pink'></div>");
