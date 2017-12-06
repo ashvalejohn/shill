08716ed9a7ed32d7d9d098d47d416dab1111a106
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-06 09:44:03 -0800

    Only display cards with images

--- shill.js
+++ shill.js
@@ -21,20 +21,35 @@ port.postMessage({ urls: links });
 // STEP 6
 // Receive card information
 port.onMessage.addListener((card) => {
-  const cardInfo = {};
+  const cardInfo = {
+    description: '',
+  };
+  console.log(card);
   const ogTags = card.metaTags.filter(attrs => attrs.property.includes('og:'));
+
   ogTags.forEach( tag => {
     if (tag.property === 'og:title') {
+      if (tag.content !== undefined) {
         cardInfo['title'] = tag.content;
+      }
     } else if (tag.property === 'og:image') {
+      if (tag.content.slice(0, 4) === 'http'){
         cardInfo['imgSrc'] = tag.content;
+      }
     } else if (tag.property === 'og:description') {
-      cardInfo['description'] = tag.content;
+      if (tag.content !== undefined){
+        cardInfo['description'] = tag.content;
+      }
     }
   });
 
-  const shillCards = document.getElementById('shill-cards');
-  shillCards.insertAdjacentHTML('afterbegin', `<div class='shill-card'><a href='${card.url}' target='_blank'><img src='${cardInfo.imgSrc}'/></a><a href='${card.url}'><h1>${cardInfo.title}</h1></a><p>${cardInfo.description}</p></div>`);
+
+  if (cardInfo.imgSrc === undefined || cardInfo.imgSrc === '') {
+    console.log('Card missing information')
+  } else {
+    const shillCards = document.getElementById('shill-cards');
+    shillCards.insertAdjacentHTML('afterbegin', `<div class='shill-card'><a class='shill-card-img' href='${card.url}' target='_blank' style="background-image: url('${cardInfo.imgSrc}')" alt='${cardInfo.title}'></a><a href='${card.url}'><h1 class='shill-title'>${cardInfo.title}</h1></a><p class='shill-description'>${cardInfo.description}</p></div>`);
+  }
 });
 
 
