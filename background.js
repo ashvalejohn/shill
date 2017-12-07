f076cf16e1074a4338669dc9de289ab6887797a0
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-07 10:05:25 -0800

    Remove open port, replace with individual messages between content script and background

--- background.js
+++ background.js
@@ -1,62 +1,64 @@
-// STEP 1
-// Open extension if user is on a YouTube page
-const onYouTube = [
-  {
-    conditions: [
-      new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { urlContains: 'www.youtube.com' },
-        css: ["#description"]
-      })
-    ],
-    actions: [
-      new chrome.declarativeContent.ShowPageAction(),
-      new chrome.declarativeContent.RequestContentScript({ "js": ["shill.js"] })
-    ]
-  }
-];
+// STEP 1: Open extension if user is on a YouTube page
+  const onYouTube = [
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { urlContains: 'www.youtube.com' },
+          css: ["#description"]
+        })
+      ],
+      actions: [
+        new chrome.declarativeContent.ShowPageAction(),
+        new chrome.declarativeContent.RequestContentScript({ "js": ["shill.js"] })
+      ]
+    }
+  ];
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules(onYouTube);
+  chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+      chrome.declarativeContent.onPageChanged.addRules(onYouTube);
+    });
   });
+
+// Receive message that description is loaded
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
+  if (request.status === "descriptionLoaded"){
+    console.log("Description box is loaded");
+    console.log("Request links");
+    sendResponse({ status: "grabLinks" });
+  } else if (request.links){
+    console.log("Received links");
+    console.log(request.links);
+    sendResponse({ status: "linksReceived" });
+  }
 });
+
+ 
+// Send message to content_script.js to grab description links
+    // chrome.tabs.executeScript({ file: "shill.js" }, function(){
+    //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //     chrome.tabs.sendMessage(tabs[0].id, { status: "grabLinks" }, function (response) {
+    //       console.log(response.links);
+    //     });
+    //   });
+    // })
+
+
 
 // STEP 4
 // Receive product link URLs, send xhr request for og: tags
-chrome.runtime.onConnect.addListener(function(port) {
-  port.onMessage.addListener((msg) => {
-    console.log(`Received ${msg.urls.length} links from shill.js`);
-    
-    msg.urls.map((url) => {
-      const req = new XMLHttpRequest();
-      req.open('GET', `${url}`, true);
-      req.responseType = "document";
-      req.onreadystatechange = function () {
-        if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
-          const doc = req.response;
-          if (doc) {
-            let metaTags = [];
-            const meta = doc.querySelectorAll('meta');
-            meta.forEach((tag) => {
-              if (tag.hasAttribute("property")) {
-                let props = {}
-                const attrs = Array.from(tag.attributes);
-                attrs.map((attr) => {
-                  props[attr.name] = attr.value;
-                });
-                metaTags.push(props);
-              }
-            });
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   if (request.links === )
+//   port.onMessage.addListener((msg) => {
+//     console.log(`Received ${msg.urls.length} links from shill.js`);
 
-            // STEP 5
-            // Send cardInfo to content_script
-            if (metaTags.length > 0) {
-              port.postMessage({ url, metaTags })
-            }
-          }
-        }
-      }
-      req.send();
-    });
-  });
-});
+//     msg.urls.map((url) => {
+//       const req = new XMLHttpRequest();
+//       req.open('HEAD', `${url}`, true);
+//       console.log(req);
+//       req.onload = function () {
+//         console.log(req.response;)
+//       });
+//       req.send();
+//     });
+//   });
--- shill.js
+++ shill.js
@@ -1,56 +1,85 @@
 console.log("RUN shill.js");
-const port = chrome.runtime.connect({name: 'shill'});
-const desc = document.getElementById("description");
-desc.insertAdjacentHTML('afterbegin', "<div id='shill-cards'></div>");
-document.querySelector('head').insertAdjacentHTML('afterbegin', `<link rel='stylesheet' type='text/css' href='${chrome.runtime.getURL("shill-style.css")}'>`);
+let pageStatus = "default";
 
-// STEP 2
-// Grab all links in the #description box
-const descLinks = Array.from(document.querySelectorAll("#description > a"));
-const links = descLinks.map(link => (
-  link.textContent
-));
+chrome.runtime.sendMessage({ status: "descriptionLoaded" }, function (response) {
+  pageStatus = response.status;
+  checkStatus();
+});
 
-if (links) {
-  console.log("shill.js grabbed links from description");
+function checkStatus(){
+  switch(pageStatus) {
+    case "grabLinks":
+      console.log("Calling grabLinks()");
+      grabLinks();
+    case "linksReceived":
+      console.log("Background received links");
+    default:
+      null;
+  }
 }
 
-// STEP 3
-// Send URLs to background as message
-port.postMessage({ urls: links });
+function grabLinks(){
+  const desc = document.getElementById("description");
+  desc.insertAdjacentHTML('afterbegin', "<div id='shill-cards'></div>");
 
-// STEP 6
-// Receive card information
-port.onMessage.addListener((card) => {
-  const cardInfo = {
-    description: '',
-  };
+  document.querySelector('head').insertAdjacentHTML('afterbegin', `<link rel='stylesheet' type='text/css' href='${chrome.runtime.getURL("shill-style.css")}'>`);
 
-  const ogTags = card.metaTags.filter(attrs => attrs.property.includes('og:'));
+  const descLinks = Array.from(document.querySelectorAll("#description > a"));
 
-  ogTags.forEach(tag => {
-    if (tag.property === 'og:title') {
-      if (tag.content !== undefined) {
-        cardInfo['title'] = tag.content;
-      }
-    } else if (tag.property === 'og:image') {
-      if (tag.content.slice(0, 4) === 'http') {
-        cardInfo['imgSrc'] = tag.content;
-      }
-    } else if (tag.property === 'og:description') {
-      if (tag.content !== undefined) {
-        cardInfo['description'] = tag.content;
-      }
-    }
+  const links = descLinks.map(link => (
+    link.textContent
+  ));
+  
+  chrome.runtime.sendMessage({ links: links }, function (response) {
+    pageStatus = response.status;
+    checkStatus();
   });
+}
 
 
-  if (cardInfo.imgSrc === undefined || cardInfo.imgSrc === '') {
-    console.log('Card missing information')
-  } else {
-    // STEP 7
-    // Display product card
-    const shillCards = document.getElementById('shill-cards');
-    shillCards.insertAdjacentHTML('afterbegin', `<div class='shill-card'><a class='shill-card-img' href='${card.url}' target='_blank' style="background-image: url('${cardInfo.imgSrc}')" alt='${cardInfo.title}'></a><a href='${card.url}'><h1 class='shill-title'>${cardInfo.title}</h1></a><p class='shill-description'>${cardInfo.description}</p></div>`);
-  }
-});
+
+// if (links) {
+//   console.log("shill.js grabbed links from description");
+// }
+
+// // STEP 3
+// // Send URLs to background as message
+// port.postMessage({ urls: links });
+
+// // STEP 6
+// // Receive card information
+// port.onMessage.addListener((card) => {
+//   const cardInfo = {
+//     description: '',
+//   };
+
+//   const ogTags = card.metaTags.filter(attrs => attrs.property.includes('og:'));
+
+//   ogTags.forEach(tag => {
+//     if (tag.property === 'og:title') {
+//       if (tag.content !== undefined) {
+//         cardInfo['title'] = tag.content;
+//       }
+//     } else if (tag.property === 'og:image') {
+//       if (tag.content.slice(0, 4) === 'http') {
+//         cardInfo['imgSrc'] = tag.content;
+//       }
+//     } else if (tag.property === 'og:description') {
+//       if (tag.content !== undefined) {
+//         cardInfo['description'] = tag.content;
+//       }
+//     }
+//   });
+
+
+//   if (cardInfo.imgSrc === undefined || cardInfo.imgSrc === '') {
+//     console.log('Card missing information')
+//   } else {
+//     // STEP 7
+//     // Display product card
+//     const shillCards = document.getElementById('shill-cards');
+//     shillCards.insertAdjacentHTML('afterbegin', `<div class='shill-card'><a class='shill-card-img' href='${card.url}' target='_blank' style="background-image: url('${cardInfo.imgSrc}')" alt='${cardInfo.title}'></a><a href='${card.url}'><h1 class='shill-title'>${cardInfo.title}</h1></a><p class='shill-description'>${cardInfo.description}</p></div>`);
+//   }
+// });
+
+
